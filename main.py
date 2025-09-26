4f6e936396504c584c4c54652671d6b0f9da0ded
Ana Sousa <cleideana520@gmail.com>
2025-09-26 13:02:45 +0000

    Código da aula 19/09

--- main.py
+++ main.py
@@ -2,10 +2,42 @@
 # Revisão de Orientação a Objetos
 # Prof. Guilherme Leal Santos
 
-frutas = ['Maça', 'Banana', 'Laranja'] 
+# Aula 19/09 - Orientação a Objetos
+from conta import Conta
+from cliente import Cliente
+
+cliente1 = Cliente('Elvis Presley', '111.222.333-44')
+conta1 = Conta(cliente1, 1, 123, 'elvis@gmail.com', 12345678)
+conta1.extrato()
+# conta1.deposita(100)
+# conta1.extrato()
+
+# conta2 = conta1
+# conta2.extrato()
+# conta2.saca(100)
+# conta2.extrato()
+# conta1.extrato()
+
+# if(conta1.saca(1000)):
+#     print('OK')
+# else:
+#     print('Tá Liso')
+
+cliente2 = Cliente('Jonhny Cage', '222.333.444-55')
+conta2 = Conta(cliente2, 2, 234, 'jonhnny@outlook.com', 234567)
+conta2.extrato()
+
+if(conta2.transfere(conta1, 1000)):
+    print('OK')
+else:
+    print('Tá liso')
+
+'''
+# Aula 12/092023 - Listas e Funções Lambda
+frutas = ['Maçã', 'Banana', 'Laranja']
 print(frutas)
 print(frutas[0])
-print(f'Tamanho: (len(frutas))')
+print(f'Tamanho: {len(frutas)}')
 
 frutas.append('Uva')
 print(frutas)
@@ -13,42 +45,45 @@ print(frutas)
 frutas.insert(0, 'Abacaxi')
 print(frutas)
 
-# fruta = frutas.pop()
-# -> Remove ultimo elemento da lista
+# -> Remove último elemento da lista
+# fruta = frutas.pop() 
+# -> Remove elemento do índice 0
 # fruta = frutas.pop(0)
-frutas.remove('Kiwi')
+frutas.remove('Laranja')
 # print(f'Removido: {fruta}')
 print(frutas)
 
-numeros = [3, 1, 4, 1, 5, 9, 2, 6, 5] 
+numeros = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 print(numeros)
 
-# Ordenar - decrecente 
-numeros_ord_d = sorted(numeros, reverse=True)
-print(f'Lista ordenada (d): {numeros_ord_d}') 
+# Ordenar - crescente
+numeros_ord_c = sorted(numeros)
+print(f'Lista ordenada (c): {numeros_ord_c}')
 
-numeros_dobrados = []
-for n in numeros:
-    numeros_dobrados.append(n * 2)
-numeros_dobrados = []
-for n in numeros:
-    numeros_dobrados.append(n*2)
-numeros_dobrados = (map(lambda n: n*2, numeros))
+# Ordenar - decrescente
+numeros_ord_d = sorted(numeros, reverse=True)
+print(f'Lista ordenada (d): {numeros_ord_d}')
+
+# numeros_dobrados = []
+# for n in numeros:
+#     numeros_dobrados.append(n*2)
+numeros_dobrados = list(map(lambda n: n*2, numeros))
 print(numeros_dobrados)
 
-numeros_filtrados = []
-for n in numeros:
-    if n > 4:
-        numeros_filtrados.append(n) 
-numeros_filtrados = list(filter(lambda n: n>4, numeros))
+# numeros_filtrados = []
+# for n in numeros:
+#     if n > 4:
+#         numeros_filtrados.append(n)
+numeros_filtrados = list(filter(lambda n: n > 4, numeros))
 print(numeros_filtrados)
 
-soma = 0 
+soma = 0
 for n in numeros:
     soma += n
 print(soma)
 
 from functools import reduce
 
-soma = reduce(lambda  soma, n: soma + n, numeros, 1)
+soma = reduce(lambda soma, n: soma + n, numeros)
 print(soma)
+'''
